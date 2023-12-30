acc407fe33cc348f2e69e2eb90cc4d56090f607a
Abdalrahman Alasil <202202066@zewailcity.edu.eg>
2023-12-30 21:57:52 +0200

    .

--- Pages/AddProject.cshtml.cs
+++ Pages/AddProject.cshtml.cs
@@ -1,12 +1,45 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Xml.Linq;
 
 namespace Project_test.Pages
 {
     public class AddProjectModel : PageModel
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
         public void OnGet()
         {
+        }
+        public void OnPost()
+        {
+            try
+            {
+                string connectionString = "Data Source=Alasil;Initial Catalog=JOpera;Integrated Security=True";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "INSERT INTO Project" + "(/*FreeLancerID,*/ Name, Description) VALUES" + "" +
+                        "(/*@FreeLancerID,*/ @Name, @Description)";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        //command.Parameters.AddWithValue("@FreeLancerID", 100);
+                        command.Parameters.AddWithValue("@Name", Request.Form["name"].ToString());
+
+                        command.Parameters.AddWithValue("@Description", Request.Form["Description"].ToString());
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            Response.Redirect("/UserInfo");
         }
     }
 }
