70b474f733c4087250ff105bbcbdf7472f418ea5
Abdalrahman Alasil <202202066@zewailcity.edu.eg>
2024-01-03 14:41:25 +0200

    .

--- Pages/Products.cshtml.cs
+++ Pages/Products.cshtml.cs
@@ -1,12 +1,67 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace Project_test.Pages
 {
     public class ProductsModel : PageModel
     {
-        public void OnGet()
+        public List<Productssinfo> products = new List<Productssinfo>();
+        private readonly ILogger<ProductsModel> _logger;
+
+        public ProductsModel(ILogger<ProductsModel> logger)
         {
+
+            _logger = logger;
+        }
+        public void OnGet(string category)
+        {
+            try
+            {
+                string connectionString = "Data Source=Alasil;Initial Catalog=JOpera;Integrated Security=True";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT [Name],[Price],[Category] FROM Product";
+
+                    if (!string.IsNullOrEmpty(category))
+                    {
+                        query += $" WHERE Category = @category";
+                    }
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        if (!string.IsNullOrEmpty(category))
+                        {
+                            command.Parameters.AddWithValue("@category", category);
+                        }
+
+                        using (SqlDataReader data = command.ExecuteReader())
+                        {
+                            while (data.Read())
+                            {
+                                Productssinfo info = new Productssinfo();
+                                info.Price = "" + data.GetInt32(1);
+                                info.Name = data.GetString(0);
+                                info.Category = data.GetString(2);
+                                products.Add(info);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public class Productssinfo
+        {
+            public string Name;
+            public string Price;
+            public string Category { get; set; }
         }
     }
 }
--- Pages/Services.cshtml.cs
+++ Pages/Services.cshtml.cs
@@ -1,12 +1,66 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
 
 namespace Project_test.Pages
 {
     public class ServicesModel : PageModel
     {
-        public void OnGet()
+        public List<ServicesInfo> Services = new List<ServicesInfo>();
+        private readonly ILogger<ProductsModel> _logger;
+
+        public ServicesModel(ILogger<ProductsModel> logger)
         {
+
+            _logger = logger;
+        }
+        public void OnGet(string category)
+        {
+            try
+            {
+                string connectionString = "Data Source=Alasil;Initial Catalog=JOpera;Integrated Security=True";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT [Name],[Price],[Category] FROM Service";
+
+                    if (!string.IsNullOrEmpty(category))
+                    {
+                        query += $" WHERE Category = @category";
+                    }
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        if (!string.IsNullOrEmpty(category))
+                        {
+                            command.Parameters.AddWithValue("@category", category);
+                        }
+
+                        using (SqlDataReader data = command.ExecuteReader())
+                        {
+                            while (data.Read())
+                            {
+                                ServicesInfo info = new ServicesInfo();
+                                info.Price = "" + data.GetInt32(1);
+                                info.Name = data.GetString(0);
+                                info.Category = data.GetString(2);
+                                Services.Add(info);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public class ServicesInfo
+        {
+            public string Name;
+            public string Price;
+            public string Category { get; set; }
         }
     }
 }
